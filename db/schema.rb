779b39d896ea3936c2646835b39799fae694b4fe
Bbosa Muhamood <44940073+bmuhamood@users.noreply.github.com>
2022-06-07 16:23:56 -0700

    created db connection with Model snd list of articles in the console

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_224757) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
 end
