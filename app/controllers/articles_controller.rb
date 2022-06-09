749fb519845e80f6adfb486944301a3d30f8d67f
Bbosa Muhamood <44940073+bmuhamood@users.noreply.github.com>
2022-06-08 19:26:02 -0700

    add save article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,25 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'articles', to: 'articles#index'
-  resources :articles#, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create ]
 end
