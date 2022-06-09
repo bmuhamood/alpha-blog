e06332766a4be790f790d9d3ca8ca65d402d4acc
Bbosa Muhamood <44940073+bmuhamood@users.noreply.github.com>
2022-06-08 19:06:10 -0700

    add articles listing feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'articles', to: 'articles#show'
-  resources :articles, only: [:show]
+  get 'articles', to: 'articles#index'
+  resources :articles#, only: [:show]
 end
